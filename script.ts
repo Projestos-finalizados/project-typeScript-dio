09d65bb001b4768813a994437f8c5df011343b9d
Mario <signorelii2308@gmail.com>
2022-04-28 10:35:28 -0300

    interfece typescript

--- script.ts
+++ script.ts
@@ -1,3 +1,34 @@
 (function(){
+const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+function patio(){
 
+function ler(){}
+
+function adcionar(){}
+
+function salvar(){}
+
+function remover(){}
+
+function render(){}
+
+return{ ler, adcionar, salvar, remover, render}
+
+
+
+
+
+
+
+
+}
+
+$ ('#cadastrar')?.addEventListener('click', ()=> {
+    const nome = $('#nome')?.value;
+    const placa = $('#placa')?.value;
+     if (nome  || placa) {
+         alert(' Nome e placa são obrigatorio')
+         return;
+     }
+})
 })();
